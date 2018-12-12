4a014dae929835e47c5a0fe958770a2aa8b4140d
notoria <notoria@users.noreply.github.com>
2018-12-12 15:53:10 +0100

    Update

--- F.m
+++ F.m
@@ -1,4 +1,13 @@
-function Q_prime = F(Q, alpha, theta)
-    Q_prime(1:3) = Q(4:6);
-    Q_prime(4:6) = -mu * Q(1:3) / norm(Q(1:3), 2)^3 + alpha(1) * u +
+function Q_prime = F(Q, i, alpha, v_e, M, theta, c_x, rho_0, R_t)
+    % i: etape
+    % alpha: acceleration initiale
+    % v_e: vitesse d'ejection
+    % M: masse initiale a l'etape i
+    % theta: angles
+    % c_x: coefficient de trainee du lanceur
+    % rho_0: densite de l'air au sol
+    % R_t: rayon terrestre
+    Q_prime(1:2) = Q(3:4);
+    Q_prime(3:4) = -mu * Q(1:2) / norm(Q(1:2), 2)^3 + alpha(i) * u - c_x * rho_0 * exp(-(norm(Q(1:2), 2) - R_t) / Q(5) * norm(Q(3, 4) * Q(3, 4)
+    Q_prime(5) = -alpha(i) / v_e(i) * M(i);
 end
--- ode_integration.m
+++ ode_integration.m
@@ -1,9 +1,11 @@
 function []=ode_integration(R_0, v_0, M_0, theta)
     steps = [15, 2600; 10, 3000; 10, 4400];
     k = 0;
+    R_0 = [R_t; 0];
     hold on;
     while k < length(steps)
-        [t, Q] = ode45(@(t, Q) [Q(4); Q(5); Q(6); 0
+        f = @(t, q) F(q, k + 1, step(1:3, 1), step(1:3, 2), theta, c_x, rho_0, R_t);
+        [t, Q] = ode45(f, [R_0, V(k + 1), M(k + 1)], 
     end
     hold off;
 end
@@ -18,4 +20,5 @@ end
 % R_0 = [R_t; 0];
 % V_0 = v_0 * [cos(theta); sin(theta)];
 % M_0 = 1500;
+% v_e = [2600, 3000, 4400];
 % v = @(theta_0, theta_1, theta_2, theta_3)
